aae71769af471c43eb5eb8fc39dc4ba76891c752
Rishi Raj Jain <rishi@neon.tech>
2024-11-29 11:48:06 +0530

    add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,33 @@ import Footer from '@/components/footer'
 import Post from '@/components/post'
 import PostPlaceholder from '@/components/post-placeholder'
 import { getPost, getRandomUser } from '@/lib/prisma/api'
+import { Metadata } from 'next'
 import { Suspense } from 'react'
+
+const btoa = (str: string) => Buffer.from(str).toString('base64')
+
+const config = {
+  url: 'https://ably-livesync-neon.vercel.app',
+  title: 'Real-time comments with Ably LiveSync and Postgres',
+  description: 'A demo of how Ably LiveSync can be combined with a Serverless Postgres to power real-time comments.',
+}
+
+export const metadata: Metadata = {
+  title: config.title,
+  description: config.description,
+  openGraph: {
+    url: config.url,
+    title: config.title,
+    description: config.description,
+    images: `https://neon.tech/docs/og?title=${btoa(config.title)}&breadcrumb=${btoa('Ably')}`,
+  },
+  twitter: {
+    title: config.title,
+    card: 'summary_large_image',
+    description: config.description,
+    images: `https://neon.tech/docs/og?title=${btoa(config.title)}&breadcrumb=${btoa('Ably')}`,
+  },
+}
 
 export default async function () {
   const promises = await Promise.all([getPost(1), getRandomUser()])
